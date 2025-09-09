be64555801a3e1b1a44f2d889d95960ffd339b44
mmdcloud <madmaxcloudpractice@gmail.com>
2025-09-09 23:33:48 +0530

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,137 @@
+# -----------------------------
+# Resource Group
+# -----------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-demo"
+  location = "eastus"
+}
+
+# -----------------------------
+# Virtual Network + Subnet
+# -----------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "vnet-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# -----------------------------
+# Private DNS Zone with Auto-Registration
+# -----------------------------
+resource "azurerm_private_dns_zone" "dns" {
+  name                = "internal.app"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dnslink" {
+  name                  = "dnslink"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = true
+}
+
+# -----------------------------
+# Network Security Group
+# -----------------------------
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# -----------------------------
+# Public IPs
+# -----------------------------
+resource "azurerm_public_ip" "pip" {
+  count               = 2
+  name                = "vm${count.index + 1}-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# -----------------------------
+# Network Interfaces
+# -----------------------------
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "vm${count.index + 1}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip[count.index].id
+  }
+}
+
+# Attach NSG
+resource "azurerm_network_interface_security_group_association" "nic-nsg" {
+  count                     = 2
+  network_interface_id      = azurerm_network_interface.nic[count.index].id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# -----------------------------
+# Virtual Machines
+# -----------------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = 2
+  name                = "vm${count.index + 1}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic[count.index].id,
+  ]
+
+  admin_password = "P@ssword12345!" # Not for production!
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  # Cloud-init to install Nginx & set hostname
+  custom_data = base64encode(<<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y nginx
+              echo "Hello from $(hostname)" > /var/www/html/index.html
+              systemctl enable nginx
+              systemctl start nginx
+              EOF
+  )
+}
